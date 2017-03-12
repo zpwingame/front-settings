046a80e9864ab9c8c6ea0b0cafdd01362f278a23
zhaopeng <1175420363@qq.com>
2017-03-12 16:43:16 +0800

    .editorconfig

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import './index.scss';
 import $ from 'jquery';
 
 $(function() {
-    var slide = $(".main .slide");
-    var tap = document.getElementsByClassName('tap');
-    var i = 0;
-    const b = 0;
-    var leftArray = [-300, -200, -100, 0, 100, 200, 300];
-    leftArray.forEach((item) => {
-        console.log(item);
-    });
-    if(true){
-        console.log(1);
-    }
+	var Slide = $(".main .slide");
+	console.log(Slide)
+	// var tap = document.getElementsByClassName('tap');
+	// var i = 0;
+	// const b = 0;
+	var leftArray = [-300, -200, -100, 0, 100, 200, 300];
+	leftArray.forEach((item) => {
+	    console.log(item);
+	});
+    // if(true){
+    //     console.log(1);
+    // }
 });
